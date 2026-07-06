dffb555770202c57e74f946eb0341f61f48b95fd
denshikenkyubu <xtf65amygc29prs9d6gu@docomo.ne.jp>
2026-07-06 17:15:07 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+if ("geolocation" in navigator) {
+    navigator.geolocation.getCurrentPosition(
+        function(position) {
+            const lat = position.coords.latitude;
+            const lon = position.coords.longitude;
 
+            document.getElementById("location").textContent =
+                `緯度: ${lat}\n経度: ${lon}`;
+        },
+        function(error) {
+            document.getElementById("location").textContent =
+                "位置情報を取得できませんでした。";
+        }
+    );
+} else {
+    document.getElementById("location").textContent =
+        "このブラウザは位置情報に対応していません。";
+}
